e2c0df029f1f43eefd968a6422af755072f9b117
Stephen Rudge <sbrudge@comcast.net>
2025-04-15 15:25:02 -0500

    updating the Home Component
    updated the home component to include the Category and Cuisine Dropdown sections.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,20 +1,31 @@
-import { Container, Flex } from "@mantine/core"
-import { getSpacingUnit } from "../functions/getSpacingUnit"
+import { Container, Stack, Paper, Group } from "@mantine/core"
 import { FeaturedRecipes } from "../components/featured_recipes"
-import classes from "./Home.module.css"
+import SearchBar from "../components/SearchBar/SearchBar"
+import { CategoryDropdown } from "../components/CategoryDropdown/CategoryDropdown"
+import { CuisineDropdown } from "../components"
+import styles from "./Home.module.css"
 
 export const Home = () => {
   return (
-    <Container size="xl" className={classes.container}>
-      <Flex
-        direction="column"
-        align="center"
-        justify="center"
-        gap={getSpacingUnit(2)}
-        className={classes.content}
-      >
-        <FeaturedRecipes />
-      </Flex>
+    <Container className={styles.container}>
+      <Stack gap="xl" className={styles.content}>
+        <Paper shadow="sm" p="xl" radius="lg">
+          <SearchBar />
+        </Paper>
+
+        <Group gap="md" grow>
+          <Paper shadow="sm" p="md" radius="md">
+            <CategoryDropdown />
+          </Paper>
+          <Paper shadow="sm" p="md" radius="md">
+            <CuisineDropdown />
+          </Paper>
+        </Group>
+
+        <Paper shadow="sm" p="xl" radius="lg">
+          <FeaturedRecipes />
+        </Paper>
+      </Stack>
     </Container>
   )
 }
