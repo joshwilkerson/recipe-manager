0d32d6f38d5e293e4b92dfadf9fd3391165410ed
Stephen Rudge <sbrudge@comcast.net>
2025-04-15 15:21:09 -0500

    cleaning up some code to make it DRY

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,16 +1,10 @@
 import { Flex, Title } from "@mantine/core"
 import { getSpacingUnit } from "../functions/getSpacingUnit"
-import SearchBar from "../components/SearchBar/SearchBar"
-import { CategoryList } from "../components/CategoryList/CategoryList"
-import { AreaList } from "../components/AreaList/AreaList"
 
 export const Profile = () => {
   return (
     <Flex direction="column" align="start" gap={getSpacingUnit(2)}>
-      <Title order={1}>Recipes</Title>
-      <SearchBar />
-      <CategoryList />
-      <AreaList />
+      <Title order={1}>Profile</Title>
     </Flex>
   )
 }
